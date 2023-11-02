a9b41629e4cefc931875fbcefb2187179d29a4c8
Gabriel Leonel <gabriel.leonel2016@gmail.com>
2023-11-02 16:31:04 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
 
   plugins: [
     new htmlWebpackPlugin({
-      template: path.resolve(__dirname, "index.html"),
+      template: path.resolve(__dirname, "src", "index.html"),
     }),
     new CleanWebpackPlugin(),
     new DefinePlugin({
